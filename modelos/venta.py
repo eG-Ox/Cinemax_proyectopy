0c21caf09fe5a12abc4476f4e0d51caedd52c78b
eG-Ox <marco.minanoh@istpargentina.edu.pe>
2026-07-31 13:25:42 -0500

    venta modelo agregado

---
+++ modelos/venta.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+
+class Venta:
+    def __init__(self, id_usuario, fecha_compra):
+        self.id = None
+        self.id_usuario = id_usuario
+        self.fecha_compra = fecha_compra
+
+    def __str__(self):
+        return f"[{self.id}] Usuario={self.id_usuario} | {self.fecha_compra.strftime('%Y-%m-%d %H:%M:%S')}"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "id_usuario": self.id_usuario,
+            "fecha_compra": self.fecha_compra.isoformat(timespec="seconds"),
+        }
+
+    @classmethod
+    def from_dict(cls, datos):
+        venta = cls(datos["id_usuario"], datetime.fromisoformat(datos["fecha_compra"]))
+        venta.id = datos.get("id")
+        return venta
